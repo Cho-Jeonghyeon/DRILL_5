3e72798bbe0879f25df5c1dda4a13c9ac0a2fb3a
Cho-Jeonghyeon <titanfall120@naver.com>
2025-09-25 23:08:05 +0900

    4. 상, 하 움직임 구현 remember 변수로 이전 움직임에서의 방향을 기억하고 상,하로 움직일때 방향 반영

--- move_character_with_key.py
+++ move_character_with_key.py
@@ -7,9 +7,11 @@ character = load_image('animation_sheet.png')
 
 
 def handle_events():
-    global running, dir
+    global running, dirx, diry
     global motion
-    # 300 숨쉬는 모션 (우) 200 숨쉬는 모션 (좌) 100 달리는 모션 (우) 0 달리는 모션 (좌)
+    # 300 숨쉬는 모션 (우) /200 숨쉬는 모션 (좌)
+    # 100 달리는 모션 (우) /0 달리는 모션 (좌)
+    global remember
 
     events = get_events()
     for event in events:
@@ -18,37 +20,65 @@ def handle_events():
 
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir += 1
+                dirx += 1
                 motion = 100
             elif event.key == SDLK_LEFT:
-                dir -= 1
+                dirx -= 1
                 motion = 0
+            elif event.key == SDLK_UP:
+                diry += 1
+                if(remember == 1):
+                    motion = 100
+                elif(remember == 0):
+                    motion = 0
+            elif event.key == SDLK_DOWN:
+                diry -= 1
+                if (remember == 1):
+                    motion = 100
+                elif (remember == 0):
+                    motion = 0
             elif event.key == SDLK_ESCAPE:
                 running = False
 
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                dir -= 1
+                dirx -= 1
                 motion = 300
+                remember = 1
             elif event.key == SDLK_LEFT:
-                dir += 1
+                dirx += 1
                 motion = 200
-
+                remember = 0
+            elif event.key == SDLK_UP:
+                diry -= 1
+                if (remember == 1):
+                    motion = 300
+                elif (remember == 0):
+                    motion = 200
+            elif event.key == SDLK_DOWN:
+                diry += 1
+                if (remember == 1):
+                    motion = 300
+                elif (remember == 0):
+                    motion = 200
 
 running = True
 x = 800 // 2
+y= 90
 frame = 0
-dir = 0
+dirx = 0
+diry = 0
 motion = 300
-
+remember = 3  #0:왼쪽, 1:오른쪽
 while running:
     clear_canvas()
     ground.draw(400, 300)
-    character.clip_draw(frame * 100, motion, 100, 100, x, 90)
+    character.clip_draw(frame * 100, motion, 100, 100, x, y)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
-    x += dir * 10
+    x += dirx * 10
+    y += diry * 10
     delay(0.07)
 
 
