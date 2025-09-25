88959b17a8c57bbed2e52d7f38b27c2aa4fcdcc8
Cho-Jeonghyeon <titanfall120@naver.com>
2025-09-25 22:29:03 +0900

    2. 이벤트 함수 업데이트 : 좌 우 키 누르고 땟을 때 방향 조정, 방향에 따라 케릭터 이동

--- move_character_with_key.py
+++ move_character_with_key.py
@@ -7,7 +7,7 @@ character = load_image('animation_sheet.png')
 
 
 def handle_events():
-    global running
+    global running, dir
 
     # fill here
 
@@ -15,12 +15,26 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        # fill here
+
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
 
 
 running = True
 x = 800 // 2
 frame = 0
+dir = 0
 
 while running:
     clear_canvas()
@@ -29,6 +43,7 @@ while running:
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
+    x += dir * 5
     delay(0.1)
 
 
