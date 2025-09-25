fa9655cbb494f30d134a7ff799ac900cd6676521
Cho-Jeonghyeon <titanfall120@naver.com>
2025-09-25 22:48:15 +0900

    3. motion변수로 좌, 우 이동중과 좌, 우 가민히 있는 애니메이션 분리

--- move_character_with_key.py
+++ move_character_with_key.py
@@ -8,8 +8,8 @@ character = load_image('animation_sheet.png')
 
 def handle_events():
     global running, dir
-
-    # fill here
+    global motion
+    # 300 숨쉬는 모션 (우) 200 숨쉬는 모션 (좌) 100 달리는 모션 (우) 0 달리는 모션 (좌)
 
     events = get_events()
     for event in events:
@@ -19,34 +19,43 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 dir += 1
+                motion = 100
             elif event.key == SDLK_LEFT:
                 dir -= 1
+                motion = 0
             elif event.key == SDLK_ESCAPE:
                 running = False
 
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 dir -= 1
+                motion = 300
             elif event.key == SDLK_LEFT:
                 dir += 1
+                motion = 200
 
 
 running = True
 x = 800 // 2
 frame = 0
 dir = 0
+motion = 300
 
 while running:
     clear_canvas()
     ground.draw(400, 300)
-    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    character.clip_draw(frame * 100, motion, 100, 100, x, 90)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
-    x += dir * 5
-    delay(0.1)
+    x += dir * 10
+    delay(0.07)
 
 
 
 close_canvas()
 
+#character.clip_draw(frame * 100, 300, 100, 100, x, 90) 숨쉬는 모션 (우)
+#character.clip_draw(frame * 100, 200, 100, 100, x, 90) 숨쉬는 모션 (좌)
+#character.clip_draw(frame * 100, 100, 100, 100, x, 90) 달리는 모션 (우)
+#character.clip_draw(frame * 100, 0, 100, 100, x, 90) 달리는 모션 (좌)
